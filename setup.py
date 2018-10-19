9f5de23a432aab5eab7842503fb4d9cadf37d4ce
cbrackle <c.brackley@ed.ac.uk>
2018-10-19 16:22:26 +0100

    Add setup file for python

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='capC-MAP',
+                 version='0.0.1',
+                 description='Analysis of Capture-C data',
+                 long_description=long_description,
+                 long_description_content_type="text/x-rst",
+                 author='Chris Brackley',
+                 author_email='C.Brackley@ed.ac.uk',
+                 url='https://git.ecdf.ed.ac.uk/cbrackle/capC-MAP',
+                 packages=['capC','capC/resources'],
+                 include_package_data=True,
+                 install_requires=[
+                     'biopython>=1.70',
+                 ],
+                 package_data = {
+                     '': ['*.txt'],
+                 },
+                 classifiers=[
+                     "Programming Language :: Python :: 2.7",
+                     "Operating System :: OS Independent",
+                 ],
+             )
